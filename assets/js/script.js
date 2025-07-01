697852ffb961d442c062711fad2539315da71199
Charles Richer <chalex.richer@gmail.com>
2025-07-01 19:54:20 +0100

    added some reference code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,118 @@
 /* Notes:
 - I will contain the javascript files
 */
+
+
+/* This is reference code
+
+function sendFetchRequest() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((data) => console.log("Fetch API:", data))
+    .catch((error) => console.error("Fetch error:", error));
+}
+
+function sendAxiosRequest() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => console.log("Axios:", response.data))
+    .catch((error) => console.error("Axios error:", error));
+}
+
+function sendAjaXRequest() {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts/1",
+    method: "GET",
+    success: function (data) {
+      console.log("jQuery.ajax:", data);
+    },
+    error: function (error) {
+      console.error("jQuery error:", error);
+    },
+  });
+}
+
+function sendXHRRequest() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log("XHR:", xhr.responseText);
+    } else {
+      console.error("XHR error:", xhr.status);
+    }
+  };
+  xhr.send();
+}
+
+document.getElementById("fetch").addEventListener("click", sendFetchRequest);
+document.getElementById("axios").addEventListener("click", sendAxiosRequest);
+document.getElementById("ajax").addEventListener("click", sendAjaXRequest);
+document.getElementById("xhr").addEventListener("click", sendXHRRequest);
+
+
+document.getElementById("fetchRepos").addEventListener("click", onfetchRepos);
+
+function onfetchRepos() {
+  const username = document.getElementById("username").value;
+
+  if (username) {
+    // GitHub API endpoint for fetching user repositories
+    const url = `https://api.github.com/users/${username}/repos`;
+
+    // Make a GET request to the GitHub API
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("GitHub user not found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        renderRepos(data);
+        renderRepos2(data);
+        renderRepos3(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  } else {
+    console.log("Please enter a GitHub username.");
+  }
+}
+
+const renderRepos = (repos) => {
+  const reposListEl = document.getElementById("repos");
+  let html = "";
+
+  //TODO what does this line do?
+  repos.forEach((repo) => {
+    const repoFullName = repo.full_name;
+
+    html += `<li>${repoFullName}</li>`;
+  });
+
+  reposListEl.innerHTML = html;
+};
+
+const renderRepos2 = (repos) => {
+  const reposListEl = document.getElementById("repos2");
+
+  //TODO what does this line do?
+  for (let i = 0; i < repos.length; i++) {
+    const repoFullName = repos[i].full_name;
+
+    const repoEl = document.createElement("li");
+    repoEl.textContent = repoFullName;
+    reposListEl.appendChild(repoEl);
+  }
+};
+
+const renderRepos3 = (repos) => {
+  const reposListEl = document.getElementById("repos3");
+
+  // TODO: what does this line do?
+  repos.map((repo) => (reposListEl.innerHTML += `<li>${repo.full_name}</li>`));
+};
+
+*/
