ae80ffc1ae9ece07a203cd986178d3626eea0071
Charles Richer <chalex.richer@gmail.com>
2025-07-03 20:36:59 +0100

    script mess has been ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,54 +26,124 @@ document.addEventListener("DOMContentLoaded", () => {
       fetch(geoUrl)
       .then((response) => response.json())
       .then((data) => {
+
+          var weatherArray = [];
+
           // Set the Latitude info
           var lat = data[0].lat;
           // Set the Longitude info
           var lon = data[0].lon;
           // Call getWeather API when the button is clicked
-          var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+          var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
           fetch(weatherUrl)
           .then((response) => response.json())
           .then((data) => {
-              // Create variables from API response
-              var cityName = data.name;
-              var cityCountry = data.sys.country;
-              var cityClouds = data.clouds.all;
-              var cityTemp = data.main.temp;
-              var cityTempFeel = data.main.feels_like;
-              var citySunrise = data.sys.sunrise;
-              var citySunset = data.sys.sunset;
-              var cityWeather = data.weather[0].main;
-              var cityWeatherDesc = data.weather[0].description;
-              var cityWindDeg = data.wind.deg;
-              var cityWindSpeed = data.wind.speed;
 
-              // Change the divs on the front page
-              document.getElementById("weatherCity").innerHTML = `${cityName}, ${cityCountry}`;
-              document.getElementById("weatherName").innerHTML = cityWeather;
-              document.getElementById("weatherDesc").innerHTML = cityWeatherDesc;
-              document.getElementById("weatherTemp").innerHTML = cityTemp;
-              document.getElementById("weatherFeels").innerHTML = cityTempFeel;
-              document.getElementById("weatherWindDeg").innerHTML = cityWindDeg;
-              document.getElementById("weatherWindSpeed").innerHTML = cityWindSpeed;
-              document.getElementById("weatherClouds").innerHTML = cityClouds;
-              document.getElementById("weatherSunrise").innerHTML = citySunrise;
-              document.getElementById("weatherSunset").innerHTML = citySunset;
-              /*
-              RESPONSE STRUCTURE:
-              <div id="weatherCity"></div>
-              <div id="weatherName"></div>
-              <div id="weatherDesc"></div>
-              <div id="weatherTemp"></div>
-              <div id="weatherFeels"></div>
-              <div id="weatherWindDeg"></div>
-              <div id="weatherWindSpeed"></div>
-              <div id="weatherClouds"></div>
-              <div id="weatherSunrise"></div>
-              <div id="weatherSunset"></div>
-              */
+            let dateData = data.list;
+
+            for (var i = 0; i < dateData.length; i++){
+
+              let currentRecord = dateData[i];
+              let currentWeather = currentRecord.weather[0].main;
+              let currentWeatherDesc = currentRecord.weather[0].description;
+              let currentTemp = currentRecord.main.temp;
+              let currentFeel = currentRecord.main.feels_like;
+              var currentWindDeg = currentRecord.wind.deg;
+              var currentWindSpeed = currentRecord.wind.speed;
+
+              let weatherInfo = {
+                city : data.city.name,
+                weather : currentWeather,
+                description : currentWeatherDesc,
+                temp : currentTemp,
+                feels : currentFeel,
+                wind : currentWindDeg,
+                windSp : currentWindSpeed
+              }
+
+              let currentDate = currentRecord.dt;
+              let date = new Date(currentDate * 1000);
+              let yy = String(date.getFullYear()).padStart(2, '0');
+              let mm = String(date.getMonth() +1).padStart(2, '0');
+              let dd = String(date.getDate()).padStart(2, '0');
+              let hh = String(date.getHours()).padStart(2, '0');
+              let min = String(date.getMinutes()).padStart(2, '0');
+              let dateFormatted = `${yy}/${mm}/${dd}`;
+              let timeFormatted = `${hh}:${min}`;
+
+
+              if (!weatherArray[dateFormatted]){
+                weatherArray[dateFormatted] = []
+              }
+              weatherArray[dateFormatted][timeFormatted] = weatherInfo;
+            }
+            
+            for (let date in weatherArray) {
+              let count = 1
+              let timesObj = weatherArray[date];
+
+              for (let time in timesObj) {
+
+                let cityCount = 0;
+                let weatherData = timesObj[time];
+
+                if(cityCount < 1){
+                  let innerHTML = `
+                    <div class="weatherCity">${weatherData.city}</div>
+                  `;
+                  console.log(innerHTML);
+                  // Create the element
+                  let newEl = document.createElement("div");
+                  if(count == 1){ 
+                    newEl.className = "carousel-item active";
+                  }else{
+                    newEl.className = "carousel-item";
+                  }
+                  
+                  // Assign innerHTML correctly
+                  newEl.innerHTML = innerHTML;
+                  // Append to your container
+                  var weatherResultInner = document.getElementById("weatherResultInner");
+                  weatherResultInner.appendChild(newEl);
+                }
+                cityCount++;
+              }
+              cityCount = 0;
+            }
+
+            for (var i = 0; i < weatherArray.length; i++){
+              // Ensure innerHTML is declared
+              let innerHTML = `
+                  <div class="weatherCity">${weatherArray.city}</div>
+              `;
+              
+              // Create the element
+              let newEl = document.createElement("div");
+              newEl.className = "carousel-item";
+
+              // Assign innerHTML correctly
+              newEl.innerHTML = innerHTML;
+
+              // Append to your container
+              var weatherResultInner = document.getElementById("weatherResultInner");
+              weatherResultInner.appendChild(newEl);
+            }
+            /*
+
+                    <div class="d-flex nowrap"
+                  <div id="weatherName"></div>
+                  <div id="weatherDesc"></div>
+                  <div id="weatherTemp"></div>
+                  <div id="weatherFeels"></div>
+                  <div id="weatherWindDeg"></div>
+                  <div id="weatherWindSpeed"></div>
+                  <div id="weatherClouds"></div>
+                  <div id="weatherSunrise"></div>
+                  <div id="weatherSunset"></div>
+                  */
               // DEBUG
+              console.log("array: ", weatherArray)
               console.log("data: ", data)
               //console.log("name: ", cityName);
               //console.log("county: ", cityCountry);
@@ -83,120 +153,6 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch((error) => console.error("Fetch error:", error));
   }
 
-  document.getElementById("getWeather").addEventListener("click", onfetchWeather);
+  document.getElementById("getWeather").addEventListener("click", onfetchWeather());
 
 });
-
-/* This is reference code
-
-function sendFetchRequest() {
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => response.json())
-    .then((data) => console.log("Fetch API:", data))
-    .catch((error) => console.error("Fetch error:", error));
-}
-
-function sendAxiosRequest() {
-  axios
-    .get("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => console.log("Axios:", response.data))
-    .catch((error) => console.error("Axios error:", error));
-}
-
-function sendAjaXRequest() {
-  $.ajax({
-    url: "https://jsonplaceholder.typicode.com/posts/1",
-    method: "GET",
-    success: function (data) {
-      console.log("jQuery.ajax:", data);
-    },
-    error: function (error) {
-      console.error("jQuery error:", error);
-    },
-  });
-}
-
-function sendXHRRequest() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log("XHR:", xhr.responseText);
-    } else {
-      console.error("XHR error:", xhr.status);
-    }
-  };
-  xhr.send();
-}
-
-document.getElementById("fetch").addEventListener("click", sendFetchRequest);
-document.getElementById("axios").addEventListener("click", sendAxiosRequest);
-document.getElementById("ajax").addEventListener("click", sendAjaXRequest);
-document.getElementById("xhr").addEventListener("click", sendXHRRequest);
-
-
-document.getElementById("fetchRepos").addEventListener("click", onfetchRepos);
-
-function onfetchRepos() {
-  const username = document.getElementById("username").value;
-
-  if (username) {
-    // GitHub API endpoint for fetching user repositories
-    const url = `https://api.github.com/users/${username}/repos`;
-
-    // Make a GET request to the GitHub API
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("GitHub user not found");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        renderRepos(data);
-        renderRepos2(data);
-        renderRepos3(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-      });
-  } else {
-    console.log("Please enter a GitHub username.");
-  }
-}
-
-const renderRepos = (repos) => {
-  const reposListEl = document.getElementById("repos");
-  let html = "";
-
-  //TODO what does this line do?
-  repos.forEach((repo) => {
-    const repoFullName = repo.full_name;
-
-    html += `<li>${repoFullName}</li>`;
-  });
-
-  reposListEl.innerHTML = html;
-};
-
-const renderRepos2 = (repos) => {
-  const reposListEl = document.getElementById("repos2");
-
-  //TODO what does this line do?
-  for (let i = 0; i < repos.length; i++) {
-    const repoFullName = repos[i].full_name;
-
-    const repoEl = document.createElement("li");
-    repoEl.textContent = repoFullName;
-    reposListEl.appendChild(repoEl);
-  }
-};
-
-const renderRepos3 = (repos) => {
-  const reposListEl = document.getElementById("repos3");
-
-  // TODO: what does this line do?
-  repos.map((repo) => (reposListEl.innerHTML += `<li>${repo.full_name}</li>`));
-};
-
-*/
